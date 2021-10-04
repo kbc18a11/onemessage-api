546aead2d6ea9e29335379c745dc10e83dd75a1c
kbc18a11 <wada121400@gmail.com>
2021-10-04 12:19:29 +0900

    init

---
+++ src/main/java/com/example/onemessageapi/OnemessageapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.onemessageapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnemessageapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnemessageapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/onemessageapi/OnemessageapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onemessageapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnemessageapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
