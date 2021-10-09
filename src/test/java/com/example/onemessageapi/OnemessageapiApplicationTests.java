16586a0c352f69ad3c70c0471f8bf8ffb9bbe48a
wadanaoumi <wada121400@gmail.com>
2021-10-10 02:02:27 +0900

    修正

--- src/test/java/com/example/onemessageapi/OnemessageapiApplicationTests.java
+++ src/test/java/com/example/onemessageapi/OnemessageapiApplicationTests.java
@@ -2,7 +2,6 @@ package com.example.onemessageapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class OnemessageapiApplicationTests {
