9e7535cfa14114f348e7a4f648311b3e9d23735e
wadanaoumi <wada121400@gmail.com>
2021-12-01 15:06:49 +0900

    テストの修正

--- src/test/java/com/example/onemessageapi/service/TwitterServiceTest.java
+++ src/test/java/com/example/onemessageapi/service/TwitterServiceTest.java
@@ -42,22 +42,24 @@ public class TwitterServiceTest {
     verify(repository, times(1)).save(twitterAccount);
   }
 
-  /*
-   * @Test public void Twitterアカウント情報の取得の検証() throws TwitterException { var user = new User();
-   * user.setId("tesetId");
-   * 
-   * var twitterAccount = new TwitterAccount(); twitterAccount.setAccessToken("testToken");
-   * twitterAccount.setSecretKey("testSecretKey"); twitterAccount.setUser(user);
-   * 
-   * // ユーザーIDが一致するデータを検索する処理をモック化
-   * when(repository.findByUserId(user.getId())).thenReturn(Optional.ofNullable(twitterAccount));
-   * 
-   * // Twitterアカウント情報取得処理をモック化 when(twitter.verifyCredentials()).thenReturn(null);
-   * 
-   * twitterService.getAccountFindByUserId(user.getId());
-   * 
-   * verify(repository, times(1)).findByUserId(user.getId()); verify(twitter,
-   * times(1)).setOAuthAccessToken( new AccessToken(twitterAccount.getAccessToken(),
-   * twitterAccount.getSecretKey())); verify(twitter, times(1)).verifyCredentials(); }
-   */
+  @Test
+  public void Twitterアカウント情報の取得の検証() throws TwitterException {
+    /*
+     * var user = new User(); user.setId("tesetId");
+     * 
+     * var twitterAccount = new TwitterAccount(); twitterAccount.setAccessToken("testToken");
+     * twitterAccount.setSecretKey("testSecretKey"); twitterAccount.setUser(user);
+     * 
+     * // ユーザーIDが一致するデータを検索する処理をモック化
+     * when(repository.findByUserId(user.getId())).thenReturn(Optional.ofNullable(twitterAccount));
+     * 
+     * // Twitterアカウント情報取得処理をモック化 when(twitter.verifyCredentials()).thenReturn(null);
+     * 
+     * twitterService.getAccountFindByUserId(user.getId());
+     * 
+     * verify(repository, times(1)).findByUserId(user.getId()); verify(twitter,
+     * times(1)).setOAuthAccessToken( new AccessToken(twitterAccount.getAccessToken(),
+     * twitterAccount.getSecretKey())); verify(twitter, times(1)).verifyCredentials();
+     */
+  }
 }
