e48539a3b6d1b5675835b2b7532758101b708925
wadanaoumi <wada121400@gmail.com>
2021-12-22 19:07:33 +0900

    DM送信APIにtwitterのDM送信機能実装

---
+++ src/main/java/com/example/onemessageapi/controller/DmController.java
@@ -0,0 +1,75 @@
+package com.example.onemessageapi.controller;
+
+import java.util.Optional;
+import java.util.stream.Collectors;
+import javax.validation.Valid;
+import com.example.onemessageapi.service.TwitterService;
+import com.example.onemessageapi.service.UserService;
+import org.apache.tomcat.jni.Address;
+import org.openapitools.api.DmApi;
+import org.openapitools.model.PostDmRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.server.ResponseStatusException;
+import twitter4j.TwitterException;
+
+@RestController
+public class DmController implements DmApi {
+  @Autowired
+  private final NativeWebRequest request;
+
+  @Autowired
+  private TwitterService twitterService;
+
+  @Autowired
+  private UserService userService;
+
+  public DmController(NativeWebRequest request) {
+    this.request = request;
+  }
+
+  @Override
+  public Optional<NativeWebRequest> getRequest() {
+    return Optional.ofNullable(request);
+  }
+
+  /**
+   * DMを送信する
+   */
+  @Override
+  public ResponseEntity<Void> postDm(@Valid PostDmRequest postDmRequest) {
+    // ユーザー情報の取得
+    var user = userService
+        .getLoginUser(getRequest().map(request -> (String) request.getAttribute("userId", 0)).get());
+
+    postDmRequest.getSendingAddresses().forEach(sendingAddresse -> {
+      System.err.println("ｔんぽ");
+      switch (sendingAddresse.getPlatformType()) {
+        case TWITTER:
+          try {
+            twitterService.postDm(
+                user.getTwitterAccounts(),
+                postDmRequest.getMessage(),
+                sendingAddresse
+                    .getAddresses()
+                    .stream()
+                    .mapToLong(address -> Long.parseLong(address.getId()))
+                    .toArray());
+          } catch (TwitterException e) {
+            System.err.println(e);
+
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+          }
+          break;
+
+        default:
+          throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+      }
+    });
+
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
+}
