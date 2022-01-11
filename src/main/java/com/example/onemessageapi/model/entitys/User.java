e8910ae292e39351c76498b30394d76a9f9c5c8c
wadanaoumi <wada121400@gmail.com>
2022-01-11 23:35:34 +0900

    不要なimportの削除

--- src/main/java/com/example/onemessageapi/model/entitys/User.java
+++ src/main/java/com/example/onemessageapi/model/entitys/User.java
@@ -6,7 +6,6 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
