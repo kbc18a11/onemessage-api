62aea80b0d377c69a0675430029badc5263a55dd
wadanaoumi <wada121400@gmail.com>
2022-01-20 02:45:29 +0900

    コメントの追加

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-# プロセスの削除
+# プロセスの削除   grep -v "grep"は、grepコマンドのプロセスを対象外にするためのもの
 PID=$(/bin/ps -fu $USER | grep "java -jar target/onemessageapi-0.0.1-SNAPSHOT.jar" | grep -v "grep" | awk '{print $2}')
-
+echo $PID
 if [ -n "$PID" ]; then
   kill $PID
 fi
