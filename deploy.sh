2604c876c75d3a4e7a32ebe1e548280cc7994d3f
wadanaoumi <wada121400@gmail.com>
2022-01-20 02:42:32 +0900

    deploy用シェルスクリプトにプロセス削除機能を実装

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,10 @@
+# プロセスの削除
+PID=$(/bin/ps -fu $USER | grep "java -jar target/onemessageapi-0.0.1-SNAPSHOT.jar" | grep -v "grep" | awk '{print $2}')
+
+if [ -n "$PID" ]; then
+  kill $PID
+fi
+
 mvn package spring-boot:repackage
 
 nohup java -jar target/onemessageapi-0.0.1-SNAPSHOT.jar &
